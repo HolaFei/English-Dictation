b3cc1c2dcae749d21201feb929ede614b58384cc
holafei <theside@qq.com>
2025-05-24 14:40:28 +0800

    Updates

---
+++ assets/2.A_Better_Way_To_Learn_English_2-OgnU4vFW.js
@@ -0,0 +1,11 @@
+const e=`The good thing is that it doesn't have to be like this.\r
+There is nothing wrong with you. You can learn to speak English naturally and with ease.\r
+You can use English effectively in your job, travels, and studies.\r
+You can feel relaxed and confident every time you speak English.\r
+In fact, as a long-time English teacher, I've helped thousands of students all over the world become fluent and powerful English speakers.\r
+How did I do it? I did it using a teaching method I developed called Effortless English.\r
+Effortless English enables you to learn English naturally and automatically – the way children learn before they enter school.\r
+Too often, English classes get so focused on tests, textbooks, grades and "levels," students forget why they're there in the first place.\r
+They forget about the real world goals of a more successful career and exciting international travel.\r
+With Effortless English you never lose sight of the fact that the ultimate goal of learning a language is communication.\r
+Instead, you learn to speak English both quickly and with more precision.`;export{e as default};
