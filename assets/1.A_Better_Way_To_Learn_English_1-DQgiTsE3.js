3a543eecb2fe7c37f71b67630f1a0f8557f29d41
holafei <theside@qq.com>
2025-05-24 11:44:57 +0800

    Updates

---
+++ assets/1.A_Better_Way_To_Learn_English_1-DQgiTsE3.js
@@ -0,0 +1,16 @@
+const e=`If you've picked up this book, chances are you've wanted to speak English for a while. Maybe you've even taken classes.\r
+You probably need English to improve your career.\r
+Maybe you want to travel internationally or study abroad.\r
+You know that English is the key to international business and international travel.\r
+So let me ask you something.\r
+Do you feel nervous or shy when you try to speak English?\r
+Do you still struggle to understand what someone is saying to you despite years of study?\r
+Are you embarrassed about your pronunciation or worried you speak too slowly?\r
+Are you frustrated that despite all the time you've invested in learning English you still can't speak it?\r
+Despite your goals, is it difficult for you to actually use English in your job, travels, or studies?\r
+Do you sometimes feel that you'll never master spoken English?\r
+If you answered yes to any of these questions, you're not alone.\r
+In fact, you're fairly typical. Most English students feel this way.\r
+Most adult English learners are stressed and frustrated about their speaking ability.\r
+Some feel completely hopeless and feel they'll never be able to speak English powerfully.\r
+Not because they're bad at languages, but because, like you, they've been taught using the wrong methods.`;export{e as default};
