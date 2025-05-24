7d0e99a913c6a7ed2472771c9060212ad26ce55c
holafei <theside@qq.com>
2025-05-24 10:31:22 +0800

    Updates

---
+++ assets/1.getting_up-DLWveQfP.js
@@ -0,0 +1,23 @@
+const e=`The worst part of the day for me is definitely when I have to get up.\r
+\r
+Waking up, that I can handle. But getting up? That, I hate.\r
+\r
+The covers I have on my bed are heavy, mostly because I have a comforter as well as a light blanket.\r
+\r
+I sleep with two pillows, which for some reason have different color pillowcases. Well, at least the sheets match.\r
+\r
+I sometimes wake up before the alarm goes off.\r
+\r
+I like to keep the alarm at a low volume, with some classical music, nothing too jarring.\r
+\r
+My old roommate used to like the terrible buzzer that you find on most alarm clocks nowadays, which always used to drive me nuts.\r
+\r
+As I was saying, I sometimes wake up before the alarm, usually because of some noise outside the house- a car door slamming, an alarm going off, gunfire-well, okay, not gunfire, but man, is my neighborhood noisy!\r
+\r
+Of course, when I'm staying in a hotel, it's usually easier to just get a wake up call from the hotel than set the alarm.\r
+\r
+I'm not really an early riser, so I don't jump out of bed ready to take on the world.\r
+\r
+I get up very slowly, usually one foot on the floor at a time. Every once in a while I'll oversleep, but not too often.\r
+\r
+I really love the weekends, when I can sleep in.`;export{e as default};
---
+++ assets/2.cleaning_up-CmVs7B71.js
@@ -0,0 +1,35 @@
+const n=`I go into my bathroom sometime around 6:45 a. m.\r
+\r
+My sink and medicine cabinet are on the left when you enter my bathroom.\r
+\r
+The toilet is next to that, with the tub in front.\r
+\r
+Anyway, I turn on the lights, and try to find the mouthwash in the medicine cabinet.\r
+\r
+I pour a small amount into a cup, swish it around for 30 seconds, gargle, and spit.\r
+\r
+Not pleasant, but necessary.\r
+\r
+Then I get out the floss.\r
+\r
+When I'm done flossing, I pull out the toothbrush and the toothpaste.\r
+\r
+I brush and then it's off to the shower.\r
+\r
+I pull the shower curtain aside, step into the tub, and pull the curtain back.\r
+\r
+I turn on both the hot and the cold water, looking for the perfect temperature.\r
+\r
+I lather up with soap, put some shampoo in my hair, then rinse and dry off.\r
+\r
+Now it's shaving time.\r
+\r
+I used to own an electric razor, but I found it didn't shave close enough.\r
+\r
+So now I'm back to the old hand razor.\r
+\r
+I lather up with shaving cream, and I start to shave.\r
+\r
+I rinse the razor and throw the disposable blades in the trash.\r
+\r
+It's about 7:00 AM, and I'm on to breakfast.`;export{n as default};
