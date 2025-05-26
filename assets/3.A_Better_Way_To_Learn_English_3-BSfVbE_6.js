23a819ad03dc8aca05e0476bdb1d7126fae2c537
holafei <theside@qq.com>
2025-05-26 13:44:04 +0800

    Updates

---
+++ assets/3.A_Better_Way_To_Learn_English_3-BSfVbE_6.js
@@ -0,0 +1,22 @@
+const e=`Effortless?\r
+I understand if you're skeptical – particularly if you've been trying to learn English the traditional way.\r
+You've put in the hours: memorizing vocabulary lists, doing grammar drills, reading boring textbooks.\r
+"How?" you're thinking, "can speaking English possibly be effortless?"\r
+Believe me, I feel your pain.\r
+Back when I started teaching 15 years ago, my students were all excited to begin conversing in English.\r
+And I was excited to help them.\r
+At that time, I taught in the usual way.\r
+I used textbooks and I focused on teaching grammar.\r
+I thought this was the best way to teach, and none of my students complained.\r
+I still remember one particularly intelligent student of mine from Venezuela named Gladys.\r
+Gladys was determined to speak English well.\r
+Talk about effort!\r
+Gladys attended every one of my classes.\r
+She always sat in the center of the front row.\r
+I can still picture her eager and smiling face.\r
+She took detailed notes.\r
+She listened to every word I said.\r
+She also studied at home.\r
+Every day Gladys studied her English textbooks for four hours or more.\r
+She also tried to learn 50 new vocabulary words by memorizing word lists.\r
+Gladys was my star student and I, too, was sure she would succeed.`;export{e as default};
---
+++ assets/4.A_Better_Way_To_Learn_English_4-CZPbZArH.js
@@ -0,0 +1,14 @@
+const e=`Six months later, however, she still could barely speak English.\r
+Her speech was hesitant and unnatural.\r
+She constantly made grammar mistakes with even the simplest sentences.\r
+Her pronunciation was difficult to understand.\r
+She still thought in Spanish and tried to translate to and from English when she spoke.\r
+Worst of all, Gladys felt nervous every time she tried to speak English.\r
+Speaking English was a painful experience for her.\r
+Gladys was extremely frustrated.\r
+After so much effort, she had barely improved.\r
+As her teacher, I too was frustrated.\r
+I was sure Gladys would improve quickly and couldn't understand why she had not.\r
+I followed all of the traditional teaching methods.\r
+I used the standard textbooks and the standard classroom activities. \r
+Gladys was intelligent, disciplined and consistent, and yet her English speaking barely improved.`;export{e as default};
